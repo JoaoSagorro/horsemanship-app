c9b010ec25881d8f62949604b446279f3f8eba7b
Joao Sagorro <joao.sagorro@gmail.com>
2024-03-15 15:27:41 +0000

    started creating the routes for the pages in the application

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :school]
 
   def home
+  end
+
+  def about
+  end
+
+  def school
   end
 end
