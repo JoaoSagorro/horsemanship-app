80d1c09e7a0f00c84bd9fb6ec9c559b28976f836
Joao Sagorro <joao.sagorro@gmail.com>
2024-03-25 21:26:42 +0000

    need to come back to the booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,11 +7,12 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(set_params)
     @aula = Aula.find(params[:id])
+    raise
     @booking.aula_id = @aula.id
     @booking.user_id = current_user.id
-
     if @booking.save
       @booking.update(status: !@booking.status)
+      redirect_to aulas_path
     else
       render :new, status: :unprocessable_entity
     end
