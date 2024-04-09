1a29eb33304425d490c2f7aa1e9282a2efecc129
Joao Sagorro <joao.sagorro@gmail.com>
2024-04-09 11:01:33 +0100

    added the option to invite a user to register

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, invite_for: 2.weeks
 
   enum role: [:aluno, :professor, :admin]
 
