afdc0b5b51ed6a4b52393b1a18f9582352e2b202
Joao Sagorro <joao.sagorro@gmail.com>
2024-04-11 18:36:58 +0100

    tried to make the selection date for the class index

--- app/javascript/controllers/book_class_controller.js
+++ app/javascript/controllers/book_class_controller.js
@@ -2,14 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="book-class"
 export default class extends Controller {
-  static targets = ["toggle"]
+  static targets = ["toggle", "card"]
 
   connect() {
     console.log("You're connected")
+    console.log(this.cardTarget)
   }
 
   booked() {
     console.log("you're booked")
-    
+  }
+
+  dataSelected() {
+    console.log("Selected")
+    this.cardTarget.classList.toggle("background-change")
   }
 }
--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -9,4 +9,26 @@ export default class extends Controller {
       dateFormat: "Y-m-d"
     })
   }
+
+  // Initialize date picker
+  chooseDate() {
+    flatpickr(this.element, {
+      enableTime: false,
+      dateFormat: "Y-m-d", // Set date format
+      onClose: (selectedDates, dateStr, instance) => {
+        // Convert selected date string to Date object
+        const selectedDate = new Date(dateStr);
+        // Get the month of the selected date (0-indexed)
+        const selectedMonth = selectedDate.getMonth() + 1;
+        // Get the year of the selected date
+        const selectedYear = selectedDate.getFullYear();
+
+        // Construct URL with selected date
+        const url = `/aulas?year=${selectedYear}&month=${selectedMonth}`;
+
+        // Redirect to the page with the selected date
+        window.location.href = url;
+      }
+    });
+  }
 }
