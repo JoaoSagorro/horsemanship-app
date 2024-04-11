45e84f315f673e2a0488fdf143048e5b27e53701
Joao Sagorro <joao.sagorro@gmail.com>
2024-04-11 19:53:09 +0100

    back to selecting just one card

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,7 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
 
-
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
@@ -9,46 +8,5 @@ export default class extends Controller {
       enableTime: false,
       dateFormat: "Y-m-d"
     })
-  }
-
-  // Initialize date picker
-  chooseDate() {
-    flatpickr(this.element, {
-      enableTime: false,
-      dateFormat: "Y-m-d", // Set date format
-      onClose: (selectedDates, dateStr, instance) => {
-        // Convert selected date string to Date object
-        const selectedDate = new Date(dateStr);
-        // Get the month of the selected date (0-indexed)
-        const selectedMonth = selectedDate.getMonth() + 1;
-        // Get the year of the selected date
-        const selectedYear = selectedDate.getFullYear();
-
-        // Construct URL with selected date
-        const url = `/aulas?year=${selectedYear}&month=${selectedMonth}`;
-
-        // Redirect to the page with the selected date
-        window.location.href = url;
-      }
-    });
-  };
-
-  showClasses(event) {
-    const date = event.currentTarget.dataset.date;
-    const url = `/aulas?date=${date}`; // Replace this URL with your actual endpoint
-
-    Rails.ajax({
-      type: "GET",
-      url: url,
-      success: (data) => {
-        // Handle the successful response
-        console.log("Classes for date: ", date, data);
-        // Here, you can update the DOM to display the fetched classes
-      },
-      error: (error) => {
-        // Handle any errors
-        console.error("Error fetching classes: ", error);
-      }
-    });
   }
 }
