18c34fd67180ea61f0afb5429385163d9862ea04
Joao Sagorro <joao.sagorro@gmail.com>
2024-04-11 22:38:19 +0100

    able to select each date individually

--- app/javascript/controllers/book_class_controller.js
+++ app/javascript/controllers/book_class_controller.js
@@ -1,20 +1,44 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
+
+// // Connects to data-controller="book-class"
+// export default class extends Controller {
+//   static targets = ["toggle", "card"]
+
+//   connect() {
+//     console.log("You're connected")
+//     console.log(this.cardTarget)
+//   }
+
+//   booked() {
+//     console.log("you're booked")
+//   }
+
+//   dataSelected(day) {
+//     console.log("Selected")
+//     console.log(document.querySelector(".day-card-<%= day.strftime("%d") %>"));
+//     console.dir(day)
+//     this.cardTarget.classList.toggle("background-change")
+//   }
+// }
+
+
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="book-class"
 export default class extends Controller {
-  static targets = ["toggle", "card"]
+  static targets = ["card"];
 
   connect() {
-    console.log("You're connected")
-    console.log(this.cardTarget)
+    console.log("You're connected");
   }
 
-  booked() {
-    console.log("you're booked")
-  }
+  dataSelected(event) {
+    const card = event.currentTarget;
+    const date = card.getAttribute("data-book-class-date");
 
-  dataSelected() {
-    console.log("Selected")
-    this.cardTarget.classList.toggle("background-change")
+    console.log("Selected date:", date);
+
+    // Toggle background color or perform other actions as needed
+    card.classList.toggle("background-change");
   }
 }
