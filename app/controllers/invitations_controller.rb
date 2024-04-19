df1c79c5dd5cb93e99246250621af328140e1b55
Joao Sagorro <joao.sagorro@gmail.com>
2024-04-19 17:16:24 +0100

    some changes

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,51 +1,43 @@
 class Users::InvitationsController < Devise::InvitationsController
   before_action :configure_permitted_parameters
 
-  # def create
-  #   super do |resource|
-  #     if resource.persisted? && resource.role == "aluno"
-  #       AlunoProfile.create(
-  #         set_params
-  #       )
-  #     end
-  #   end
-  # end
+  def create
+    super do |resource|
+      if resource.persisted? && resource.role == "aluno"
+        AlunoProfile.create(
+          set_params
+        )
+      end
+    end
+  end
 
-  # def create
-  #   super do |resource|
-  #     if resource.persisted? && resource.role == "aluno"
-  #       resource.create_aluno_profile(set_params)
-  #     end
-  #   end
-  # end
+  private
 
-  # private
-
-  # def set_params
-  #   params.require(:user).permit(
-  #     :first_name,
-  #     :last_name,
-  #     :morada,
-  #     :birthdate,
-  #     :nif,
-  #     :numero_utente,
-  #     :cartao_cidadao,
-  #     :validity,
-  #     :contacto,
-  #     :encarregado_educação,
-  #     :grau_parentesco,
-  #     :contacto_emergencia,
-  #     :aulas,
-  #     :horario_preferencial,
-  #     :recolha_dados,
-  #     :fotografias_captadas
-  #   )
-  # end
+  def set_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :morada,
+      :birthdate,
+      :nif,
+      :numero_utente,
+      :cartao_cidadao,
+      :validity,
+      :contacto,
+      :encarregado_educação,
+      :grau_parentesco,
+      :contacto_emergencia,
+      :aulas,
+      :horario_preferencial,
+      :recolha_dados,
+      :fotografias_captadas
+    )
+  end
 
   protected
 
   # Permit the new params here.
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit :invite, keys: %i[role]
+    devise_parameter_sanitizer.permit(:invite, keys: [:first_name, :last_name, :role])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,12 @@ class User < ApplicationRecord
   has_many :aulas, through: :user_aulas
   has_many :bookings
   has_one :aluno_profile
-  has_many :models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, invite_for: 2.weeks
 
-  # enum role: [:aluno, :professor, :admin]
-  enum role: { aluno: 0, professor: 1, admin: 2 }
+  enum role: [:aluno, :professor, :admin]
 
   after_initialize :set_default_role, :if => :new_record?
 
